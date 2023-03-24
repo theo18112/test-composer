09cc6d57ed953d05af8b71f380a76b897cf28a9a
theo18112 <theo18112@gmail.com>
2023-03-24 16:27:48 +0700

    first commit

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+namespace Rc\Belajar;
+
+class Customer
+{
+  public function __construct(private string $name)
+  {
+    # code...
+  }
+  public function sayHello(string $name): string
+  {
+    return "Hello $name, my name is $this->name" . PHP_EOL;
+  }
+}
